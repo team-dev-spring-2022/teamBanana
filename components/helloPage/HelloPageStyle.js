c2a8f7c63cfc616b870911339aaec22cdfdede16
Den_095 <43259198+DenZv95@users.noreply.github.com>
2022-02-17 14:15:31 +0900

    fix task
    Опечатка

--- components/helloPage/HelloPageStyle.js
+++ components/helloPage/HelloPageStyle.js
@@ -1,7 +1,7 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  // @todo #4 Добавить стили для страницы профиля
+  // @todo #4 Добавить стили для начальной страницы
   container: {},
 
   logo: {},
