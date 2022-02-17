73141d95d627ce8f3c6ce45fb2aa99cf77859113
unknown <den.zv95@yandex.ru>
2022-02-17 13:54:05 +0900

    #17 Добавлены элементы начальный страницы

--- components/helloPage/HelloPageStyle.js
+++ components/helloPage/HelloPageStyle.js
@@ -2,6 +2,17 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   // @todo #4 Добавить стили для страницы профиля
+  container: {},
+
+  logo: {},
+
+  text: {},
+
+  button: {},
+
+  signInButton: {},
+
+  signUpButton: {},
 });
 
 export default styles;
