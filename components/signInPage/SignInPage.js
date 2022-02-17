855c7c9625f77fbf142fac787d70b539eee9a8ab
Arkaides <apkaides.7@gmail.com>
2022-02-17 17:32:45 +0900

    #22 Добавлены элементы страницы входа

---
+++ components/signInPage/SignInPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Image, TouchableOpacity, View, Text} from 'react-native';
+
+const SignInPage = () => {
+  // @todo #22 Добавить элементы для страницы входа.
+  return (
+    <View>
+      <Text>Sign In Page</Text>
+    </View>
+  );
+};
+
+export default SignInPage;
