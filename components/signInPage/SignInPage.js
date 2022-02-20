e2a3ed6f61d7893862e212ff17647be767f971e0
unknown <den.zv95@yandex.ru>
2022-02-20 21:58:57 +0900

    #28 Добавлены элементы на страницу авторизации

--- components/signInPage/SignInPage.js
+++ components/signInPage/SignInPage.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import {Image, TouchableOpacity, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {TouchableOpacity, TextInput, View, Text} from 'react-native';
+import styles from './SignInPageStyle';
 
 const SignInPage = () => {
-  // @todo #22 Добавить элементы для страницы входа.
+  const [login, onChangeLogin] = useState(null);
+  const [password, onChangePassword] = useState(null);
+
   return (
-    <View>
-      <Text>Sign In Page</Text>
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={onChangeLogin}
+          value={login}
+          placeholder="Email"
+          style={[styles.inputText, styles.text]}
+        />
+
+        <TextInput
+          onChangeText={onChangePassword}
+          value={password}
+          placeholder="Пароль"
+          style={[styles.inputText, styles.text]}
+        />
+      </View>
+      <TouchableOpacity style={[styles.signInButton, styles.button]}>
+        <Text style={styles.text}>Войти</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.signUpButton, styles.button]}>
+        <Text style={styles.textSignUp}>Забыли пароль?</Text>
+      </TouchableOpacity>
     </View>
   );
 };
