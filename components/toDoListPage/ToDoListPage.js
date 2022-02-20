4e859a8a579b60375a240ef36048e698c9f2d3ad
Arkaides <apkaides.7@gmail.com>
2022-02-20 18:49:11 +0900

    #35 Добавлены элементы страницы с задачами

---
+++ components/toDoListPage/ToDoListPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Image, TouchableOpacity, View, Text} from 'react-native';
+
+const ToDoListPage = () => {
+  // @todo #35 Добавить элементы для страницы регистрации.
+  return (
+    <View>
+      <Text>To Do List Page</Text>
+    </View>
+  );
+};
+
+export default ToDoListPage;
---
+++ components/toDoListPage/ToDoListPageStyle.js
@@ -0,0 +1,7 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  // @todo #35 Добавить стили для страницы регистрации.
+});
+
+export default styles;
