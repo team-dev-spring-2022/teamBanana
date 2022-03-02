7d730fe6a0e9d6b901b834515eec52e88a3760f2
vacuuminhead <pav.anast1995@gmail.com>
2022-03-02 21:41:14 +0900

    #36 стиль для страницы регистрации

--- components/signUpPage/SignUpPageStyle.js
+++ components/signUpPage/SignUpPageStyle.js
@@ -1,7 +1,38 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  // @todo #33 Добавить стили для страницы регистрации.
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  inputContainer: {
+    width: '75%',
+    marginTop: 80,
+    marginBottom: 50,
+  },
+
+  inputText: {
+    backgroundColor: '#FFFFFF',
+    margin: 10,
+    paddingLeft: 20,
+    borderRadius: 14,
+  },
+
+  text: {
+    fontSize: 18,
+    color: '#FFFFFF',
+  },
+
+  signUpButton: {
+    width: 205,
+    height: 48,
+    padding: 10,
+    borderRadius: 24,
+    borderWidth: 0.5,
+    borderColor: 'black',
+    backgroundColor: '#FFD511',
+  },
 });
 
 export default styles;
