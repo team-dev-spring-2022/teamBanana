06381c4d854eaf34b98721109825a35ff487d391
Arkaides <apkaides.7@gmail.com>
2022-04-21 13:42:10 +0900

    #91 Создан шаблон страницы добавления задач

---
+++ components/createTasksPage/CreateTasksPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import style from './CreateTasksPageStyle';
+
+const CreateTasksPage = ({navigation}) => {
+  return (
+    <View>
+      <Text>Добавить Задачу</Text>
+    </View>
+  );
+};
+
+export default CreateTasksPage;
