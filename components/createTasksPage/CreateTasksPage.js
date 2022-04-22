124d6a46a412e4129e76302581f3e4a848625399
vacuuminhead <pav.anast1995@gmail.com>
2022-04-22 16:25:16 +0900

    #100 добавлены стили на страницу добавления

--- components/createTasksPage/CreateTasksPage.js
+++ components/createTasksPage/CreateTasksPage.js
@@ -1,7 +1,15 @@
 import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, TextInput} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 import styles from './CreateTasksPageStyle';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import moment from 'moment';
+import 'moment/locale/ru';
 
 const CreateTasksPage = ({navigation}) => {
   const [text, onChangeText] = useState(null);
@@ -23,27 +31,40 @@ const CreateTasksPage = ({navigation}) => {
   };
 
   return (
-    <View>
-      <Text>Что должно быть сделано?</Text>
-      <TextInput
-        onChangeText={onChangeText}
-        value={text}
-        placeholder="Введите текст"
-      />
-      <Text>Срок</Text>
-      <Text>{date.toString()}</Text>
-      <TouchableOpacity onPress={showDatePicker}>
-        <Text>Дата</Text>
-      </TouchableOpacity>
-      <DateTimePickerModal
-        isVisible={isDatePickerVisible}
-        mode="date"
-        onConfirm={handleConfirm}
-        onCancel={hideDatePicker}
-      />
-      <TouchableOpacity onPress={() => navigation.navigate('ToDoList')}>
-        <Text style={styles.addButtonText}>Добавить задачу</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style={styles.text}>Что должно быть сделано?</Text>
+        <TextInput
+          style={[styles.inputTask, styles.text]}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder="Введите текст"
+        />
+
+        <Text style={styles.text}>Срок</Text>
+        <View style={styles.dateInputContainer}>
+          <Text style={[styles.dateText, styles.text]}>
+            {moment(date.toString()).format('dddd, D MMMM YYYY г.')}
+          </Text>
+          <TouchableOpacity style={styles.dateButton} onPress={showDatePicker}>
+            <Text>Дата</Text>
+          </TouchableOpacity>
+        </View>
+
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
+        />
+      </ScrollView>
+      <View style={styles.addButtonContainer}>
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => navigation.navigate('ToDoList')}>
+          <Text style={styles.addButtonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
